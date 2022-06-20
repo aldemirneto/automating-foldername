fb454c8b14217cda7cae268b060df3eee4016980
Aldemir Humberto Soares Neto <56364675+aldemirneto@users.noreply.github.com>
2022-06-20 16:14:07 -0300

    update

--- code.ps1
+++ code.ps1
@@ -1,34 +1,37 @@
-﻿$path =  "C:\Users\55119\Desktop\TEste"
+$path =  "C:\Users\aldemir.neto\Desktop\TEste"
 
-cd "C:\Users\55119\Desktop\TEste"
+cd "C:\Users\aldemir.neto\Desktop\teste"
 
 
 $date = (Get-Date).toString('yyyy_MM_dd')
 
-#Get-ChildItem -Recurse -Exclude SB_* -Directory| Rename-Item -NewName {'SB_' + $_.Name + '_' + $date}
-#Get-ChildItem -Recurse| Rename-Item -NewName {$_.Name -replace "[' '\{]", ''}
-#Get-ChildItem -Recurse| Rename-Item -NewName {$_.Name -replace "[-;,$\{]", '_'}
+
 
 
 
 $pathde = $path + '\*'
-Get-ChildItem -Path $pathde -Exclude *.txt -File | Remove-Item
+Get-ChildItem  *.rem -Recurse| foreach { Remove-Item -Path $_.FullName }
+Get-ChildItem  *.xyz -Recurse| foreach { Remove-Item -Path $_.FullName }
+Get-ChildItem  *.kev -Recurse| foreach { Remove-Item -Path $_.FullName }
 
 
 #PROCURA TODOS OS ARQUIVOS COM NOME IGUAL AO DE UMA PASTA E MOVE O ARQUIVO P ESSA PASTA
-Get-ChildItem  -Recurse -Directory|ForEach-Object{
+Get-ChildItem -path $path -Recurse -Directory|ForEach-Object{
     $Dir = $_.Name
-    $DirName = $_.BaseName
-    $Dirpath = $_.FullName
-    echo $Dirpath
+    $Dirname = $_.BaseName
+    $Diralvo = $_.FullName
+    $parentpath = $_.Parent
+    $Dirpath = $parentpath.FullName
+       
     Get-ChildItem  -Path $Dirpath -File | ForEach-Object{
         $FileName = $_.BaseName
         $pathFile = $_.FullName
-        if($DirNAme -eq $filename){
-            echo 'achou'
-            Move-Item  $pathFile -Destination $Dirpath
+        if($DirName -eq $filename){
+            Move-Item  $pathFile -Destination $Diralvo
             
         }
     }
 }
-
+Get-ChildItem -path $path -Recurse -Exclude SB_* -Directory| Rename-Item -NewName {'SB_' + $_.Name + '_' + $date}
+Get-ChildItem -path $path -Recurse| Rename-Item -NewName {$_.Name -replace "[' '\{]", ''}
+Get-ChildItem -path $path -Recurse| Rename-Item -NewName {$_.Name -replace "[-;,$\{]", '_'}
