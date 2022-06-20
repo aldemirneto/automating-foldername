5c20e6a9dfff89048fef45ac2d5eddf866f59647
Aldemir Humberto Soares Neto <56364675+aldemirneto@users.noreply.github.com>
2022-06-20 16:51:26 -0300

    update

--- code.ps1
+++ code.ps1
@@ -10,12 +10,13 @@ $date = (Get-Date).toString('yyyy_MM_dd')
 
 
 $pathde = $path + '\*'
+#Exclue os arquivos com extensão .rem, .kev, .xyz
 Get-ChildItem  *.rem -Recurse| foreach { Remove-Item -Path $_.FullName }
 Get-ChildItem  *.xyz -Recurse| foreach { Remove-Item -Path $_.FullName }
 Get-ChildItem  *.kev -Recurse| foreach { Remove-Item -Path $_.FullName }
 
 
-#PROCURA TODOS OS ARQUIVOS COM NOME IGUAL AO DE UMA PASTA E MOVE O ARQUIVO P ESSA PASTA
+#Search for files and folder with equal names and move the file into the folder
 Get-ChildItem -path $path -Recurse -Directory|ForEach-Object{
     $Dir = $_.Name
     $Dirname = $_.BaseName
@@ -32,6 +33,12 @@ Get-ChildItem -path $path -Recurse -Directory|ForEach-Object{
         }
     }
 }
+
+#insert AL_ on the beginning of the name and  the actual date on the end of the folder name
 Get-ChildItem -path $path -Recurse -Exclude AL_* -Directory| Rename-Item -NewName {'AL_' + $_.Name + '_' + $date}
+
+#erase the extra spaces in a folder name
 Get-ChildItem -path $path -Recurse| Rename-Item -NewName {$_.Name -replace "[' '\{]", ''}
+
+#treatment of special characteres, such as -;, replacing them with _
 Get-ChildItem -path $path -Recurse| Rename-Item -NewName {$_.Name -replace "[-;,$\{]", '_'}
